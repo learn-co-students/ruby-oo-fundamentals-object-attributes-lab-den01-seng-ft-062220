ea0f461fa70109e3cfcbf2257857f41d33dd12a6
Hyung Kim <hjkmines@gmail.com>
2020-06-25 00:56:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  
+# def initialize(name)
+#   @name = name 
+# end 
+
+def name 
+  @name   
+end 
+
+def name=(dog_name)
+  @name = dog_name 
+end 
+
+def breed 
+  @breed 
+end 
+
+def breed=(breed_type)
+  @breed = breed_type
+end 
+end 
+
+
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def name 
+    @name 
+  end 
+  
+  def name=(person_name)
+    @name = person_name
+  end 
+  
+  def job 
+    @job 
+  end 
+  
+  def job=(type_of_job)
+    @job = type_of_job
+  end 
+  
+end 
